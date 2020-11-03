fcdda8350b1ef702b4be3d4baaee5f0bfaee98f6
sergeytonchenko <randick56@gmail.com>
2020-11-03 10:52:49 +0200

    fix data.js

--- src/js/modules/data.js
+++ src/js/modules/data.js
@@ -5,41 +5,32 @@ export const collectData = function(){
             params: {
                 brand: [],
                 manufacturer: $('#marka').val(),
-                model: '',
+                model: $('#filter-model').val(),
                 year: Number($('#filter-year').val()),
                 price: [Number($('#price-from').val()), Number($('#price-to').val())]
             },
             pagination: {
                 sort: $('#sort').val(),
                 perPage: Number($('#per_page').val()),
-                page: Number($('.pages__link--active').attr('page'))
+                page: Number($('.pages__count .pages__link--active').attr('page'))
             }             
         };
 
         $('input:checkbox:checked').each(function(){
             data.params.brand.push($(this).val());
-        });
-
-        if ($('#marka').val() != '') {
-            data.params.model = $('#marka').val()
-        }
-
-
-
-
-
+        });        
         //Вывод адресной строки
         const { params, pagination } = data;
-        const merge = {...params, ...pagination};
-        merge.toString = function () {
-            return `?page=${this.page}
-                    &year=${this.year}
-                    &price=${this.price}
-                    &model=${this.model}
-                    &manufacturer=${this.manufacturer}
-                    &brand=${this.brand}
-                    &sort=${this.sort}
-                    &per-page=${this.perPage}`
+        const merge = {...params, ...pagination};        
+        merge.toString = function () {                             
+                return    `?page=${this.page}
+                                &year=${this.year}
+                                &price=${this.price}
+                                &model=${this.model}
+                                &manufacturer=${this.manufacturer}
+                                &brand=${this.brand}
+                                &sort=${this.sort}
+                                &per-page=${this.perPage}`
         }
         history.pushState({}, '', merge.toString().replace(/\s+/g, ' ').trim()) 
         
